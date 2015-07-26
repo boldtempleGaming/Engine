e5c12b56c3158dbe8e66c3aaa79913f97675275a
WinPooh32 <winpooh32@yandex.ru>
2015-07-26 13:03:30 +1000

    Added simple audio sysyem.

---
+++ src/Core/Audio.cpp
@@ -0,0 +1,126 @@
+#include "Audio.h"
+
+void Audio::Init(int alloc_channels){
+    Mix_Init(MIX_INIT_OGG);
+
+    // initialize sdl mixer, open up the audio device
+    if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+        std::cerr << " >> !WARNING! << Audio isn't initialised! " << Mix_GetError() << std::endl;
+    }
+
+    Mix_AllocateChannels(alloc_channels);
+}
+
+void Audio::Quit(){
+    Mix_CloseAudio();
+    Mix_Quit();
+}
+
+int Audio::ChannelsAllocated(){
+    return Mix_AllocateChannels(-1);
+}
+
+void Audio::Play(Mix_Chunk* sound, int loops){
+    if(sound != nullptr) {
+        Mix_PlayChannel(-1, sound, loops);
+    }
+}
+
+void Audio::Play(Mix_Music* music, int loops){
+    if(music != nullptr){
+        Mix_PlayMusic(music, loops);
+    }
+}
+
+void Audio::Play(int loops){
+    if(!IsLoaded()){
+        std::cerr << " >> !Warning! << Trying to play not loaded audio file!" << std::endl;
+        return;
+    }
+
+    switch(_type){
+        case AUDIO_SOUND:
+            int free_channel;
+            int channels;
+            channels = ChannelsAllocated();
+
+            if(!IsPlaying()) {
+                //searching for free channel to play
+                for (free_channel = 0; free_channel < channels; ++free_channel) {
+                    if (Mix_Playing(free_channel) != 1) {
+                        break; // free channel found
+                    }
+                }
+
+                _channel = free_channel; // save occupied channel
+            }else{
+                free_channel = _channel;
+            }
+
+            if(channels == 0 || free_channel >= channels){
+                std::cerr << " >> !Warning! << No free audio channels! " << std::endl;
+                return; // you can alloc new channels here :)
+            }
+
+            Mix_PlayChannel(free_channel, static_cast<Mix_Chunk*>(_audio_data), loops);
+            //Mix_ChannelFinished(&Audio::OnHalt); // set callback on channel halting
+            break;
+
+        case AUDIO_MUSIC:
+            Mix_PlayMusic(static_cast<Mix_Music*>(_audio_data), loops);
+            break;
+    }
+}
+
+Audio::Audio(Mix_Music* music){
+    _type = AUDIO_MUSIC;
+    _audio_data = music;
+}
+
+Audio::Audio(Mix_Chunk* sound){
+    _type = AUDIO_SOUND;
+    _audio_data = sound;
+}
+
+Audio::Audio(const std::string& file_path, audio_type type){
+    switch(type){
+        case AUDIO_SOUND:
+            Mix_Chunk* chunk;
+            chunk = Mix_LoadWAV(file_path.c_str());
+            if(chunk != nullptr){
+                _audio_data = chunk;
+                _type = AUDIO_SOUND;
+            }
+            break;
+        case AUDIO_MUSIC:
+            Mix_Music* music;
+            music = Mix_LoadMUS(file_path.c_str());
+            if(music != nullptr){
+                _audio_data = music;
+                _type = AUDIO_MUSIC;
+            }
+            break;
+    }
+}
+
+bool Audio::IsLoaded(){
+    return (_audio_data != nullptr);
+}
+
+bool Audio::IsPlaying(){
+    switch(_type){
+        case AUDIO_SOUND:
+            if(_channel != -1) {
+                if (Mix_Playing(_channel) && !Mix_Paused(_channel)) {
+                    return true;
+                }else{
+                    _channel = -1;
+                }
+            }
+            break;
+        case AUDIO_MUSIC:
+            return (Mix_PlayingMusic() == 1);
+            break;
+    }
+    return false;
+}
---
+++ src/Core/Audio.h
@@ -0,0 +1,42 @@
+#ifndef ENGINE_AUDIO_H
+#define ENGINE_AUDIO_H
+
+#include <iostream>
+#include <string>
+
+#include <SDL_mixer.h>
+
+enum audio_type{
+    AUDIO_SOUND = 0,
+    AUDIO_MUSIC
+};
+
+class Audio {
+public:
+    static void Init(int alloc_channels = 16);
+    static void Quit();
+
+    static int ChannelsAllocated();
+
+    static void Play(Mix_Chunk* sound, int loops = 0);
+    static void Play(Mix_Music* music, int loops = 0);
+
+    Audio(Mix_Chunk* sound);
+    Audio(Mix_Music* sound);
+    Audio(const std::string& file_path, audio_type type = AUDIO_SOUND);
+
+    void Play(int loops = 0);
+    bool IsLoaded();
+    bool IsPlaying();
+
+private:
+    static const int _max_channels;
+
+    audio_type _type;
+    void* _audio_data = nullptr; // use dyn or static cast to set type
+    int _channel = -1; // stopped
+    bool _is_playing = false;
+};
+
+
+#endif //ENGINE_AUDIO_H
