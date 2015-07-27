220ee9c85e847a3e146de109b8ab7be36c7a7d52
WinPooh32 <winpooh32@yandex.ru>
2015-07-28 01:21:06 +1000

    Drawing text by lines

--- src/GUI/Widgets/Box.cpp
+++ src/GUI/Widgets/Box.cpp
@@ -8,19 +8,31 @@
 
 #include "GUI/Widgets/Box.h"
 #include <iostream>
+#include <sstream>
 
-Box::Box(Object* owner, const Vec2& pos, const Vec2& size) :
+const int TEXT_OFFSET = 4; // pixels
+const int LINE_SPACE = 1;
+
+Box::Box(Object* owner, const Vec2& pos, const Vec2& size, const std::string& font, int font_pt_size) :
         Widget(owner, pos, size)
 {
+    ShowBack(false);
+    _text.Init(TEXT_OFFSET, TEXT_OFFSET, "", font, font_pt_size);
 
+    _text_texture = SDL_CreateTexture(Window::GetRenderer(), SDL_PIXELFORMAT_RGBA8888,
+                                      SDL_TEXTUREACCESS_TARGET, GetSize().x - TEXT_OFFSET*2,
+                                      GetSize().y - TEXT_OFFSET*2);
+    SDL_SetTextureBlendMode(_text_texture, SDL_BLENDMODE_BLEND);
+
+    SetPos(pos);
 }
 
 Box::~Box() {
-
+    SDL_DestroyTexture(_text_texture);
 }
 
 void Box::OnUpdate(){
-    Object::CheckClick();
+    //Object::CheckClick();
 }
 
 void Box::OnRender() {
@@ -28,9 +40,43 @@ void Box::OnRender() {
         if (_bg_visible){
             _back.Draw(Object::GetGlobalPos(), Object::GetSize(), Window::GetCamera() );
         }
+        Surface::Draw(_text_texture, &_text_rect);
     }
 }
 
+void Box::SetPos(const Vec2 &pos) {
+    Object::SetPos(pos);
+
+    _text_rect = {GetPos().x + TEXT_OFFSET,
+                  GetPos().y + TEXT_OFFSET,
+                  GetSize().x - TEXT_OFFSET*2,
+                  GetSize().y - TEXT_OFFSET*2};
+}
+
 void  Box::OnClick(){
-    std::cout << "YOHOHO BOX clicked: "<< GetId() << std::endl;
+    //std::cout << "YOHOHO BOX clicked: "<< GetId() << std::endl;
+}
+
+void Box::SetText(const std::string &str) {
+    //change the rendering target
+    SDL_SetRenderTarget(Window::GetRenderer(), _text_texture);
+    SDL_SetRenderDrawColor(Window::GetRenderer(), 0, 0, 0, 0); // tranceparent surface
+    SDL_RenderClear(Window::GetRenderer());
+
+    std::istringstream stream(str);
+    std::string line;
+
+    int row = TEXT_OFFSET;
+
+    while(std::getline(stream, line)){
+        _text.SetText(line);
+        _text.SetPos(TEXT_OFFSET, row);
+        _text.Draw();
+
+        row += _text.GetH() + LINE_SPACE;
+    }
+
+    //back to default target
+    SDL_SetRenderDrawColor(Window::GetRenderer(), BACKGROUND_COLOR.r, BACKGROUND_COLOR.g, BACKGROUND_COLOR.b, 255);
+    SDL_SetRenderTarget(Window::GetRenderer(), nullptr);
 }
--- src/GUI/Widgets/Box.h
+++ src/GUI/Widgets/Box.h
@@ -9,16 +9,28 @@
 #ifndef SRC_GUI_BOX_H_
 #define SRC_GUI_BOX_H_
 
+#include <string>
+
+#include "Render/Sprite.h"
 #include "GUI/Widget.h"
+#include "GUI/Text.h"
 
 class Box: public Widget {
 public:
-    Box(Object* owner, const Vec2& pos, const Vec2& size);
+    Box(Object* owner, const Vec2& pos, const Vec2& size, const std::string& font, int font_pt_size);
     virtual ~Box();
 
+    virtual void SetPos(const Vec2& pos);
     virtual void OnUpdate();
     virtual void OnRender();
     virtual void OnClick();
+
+    void SetText(const std::string& str);
+
+private:
+    SDL_Texture* _text_texture;
+    Text _text;
+    SDL_Rect _text_rect;
 };
 
 #endif /* SRC_GUI_BOX_H_ */
