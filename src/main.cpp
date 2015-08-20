fda6b6339f2fd45108811448f19e8968c38b45b8
WinPooh32 <winpooh32@yandex.ru>
2015-08-20 10:19:27 +1000

    Added test button to demo scene

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,18 @@
 #include "Demo/Platform.h"
 #include "Demo/map/Map.h"
 #include "Demo/Jim.h"
+#include "GUI/Widgets/Box.h"
+#include "GUI/Widgets/Button.h"
+
 #include "Core/FPScounter.h"
 
 using namespace std;
 //Map test_map;
 
 FPS_counter fps;
-Box* box;
+Box *box;
 Jim *jim1;
-
+Button *btn;
 void Engine::OnInit() {
     Window::SetMode(1024, 720, false);
     //U can set map size in constants.h file
@@ -40,22 +43,20 @@ void Engine::OnInit() {
     jim1 = new Jim(Vec2(5, 20));
     jim1->_is_player = true;
 
-
 /*
-    for (int i = 1; i < 100; i++) {
+    for (int i = 1; i < 500; i++) {
         Jim *jim = new Jim(Vec2(160*i, 20));
         Engine::GetRoot()->Connect(jim);
     //jim->Connect(new Jim(Vec2(100, 0)));
     //area->Connect(new Jim(Vec2(0, 0)));
-    }
-*/
+    }*/
 
     Engine::GetRoot()->Connect(new Platform(Vec2(0, 400), Vec2(128, 32)));
     Engine::GetRoot()->Connect(new Platform(Vec2(150, 400), Vec2(128, 32)));
     Engine::GetRoot()->Connect(new Platform(Vec2(300, 400), Vec2(128, 32)));
 
     Engine::GetRoot()->Connect(new Platform(Vec2(215, 400), Vec2(128, 32)));
-    Engine::GetRoot()->Connect(new Platform(Vec2(710, 400), Vec2(128, 200)));
+    Engine::GetRoot()->Connect(new Platform(Vec2(710, 400), Vec2(5000, 200)));
 
     Engine::GetRoot()->Connect(new Platform(Vec2(350, 100), Vec2(128, 300)));
     Engine::GetRoot()->Connect(new Platform(Vec2(200, 200), Vec2(128, 32)));
@@ -63,44 +64,50 @@ void Engine::OnInit() {
 
     Engine::GetRoot()->Connect(jim1);
 
-
     box = new Box(Engine::GetRoot(), Vec2(0, 0), Vec2(160, 64), "PressStart2P.ttf", 8);
     box->SetText("Hey, world!\nА это по-русски!\nHEY again");
+
+    //Window::GetCamera()->SetViewport(Vec2(800, 600));
+
+    btn = new Button(Engine::GetRoot(), Vec2(100, 20), Vec2(160, 64), "PressStart2P.ttf", 12);
+    btn->SetStyle("pgui.png", Vec2(0,0), Vec2(24, 24), 8);
+    btn->SetText("Button!\nBlaBlaBla");
+    //btn->SetBackGround("pgui.png", Vec2(), 8);
 }
 
-void Engine::OnEvent(SDL_Event* event, const Uint8* keyboardState) {
+void Engine::OnEvent(SDL_Event *event, const Uint8 *keyboardState) {
     if (event->type == SDL_MOUSEWHEEL) {
         //on mouse wheel event
     } else if (event->type == SDL_KEYDOWN) {
         //Move camera w,a,s,d
 
-        Camera* cam = Window::GetCamera();
+        Camera *cam = Window::GetCamera();
 
         int code = event->key.keysym.scancode;
 
-        switch(code){
+        switch (code) {
 
             case SDL_SCANCODE_R:
                 jim1->SetPos(Vec2(5, 20));
                 break;
-        case SDL_SCANCODE_D:
-            cam->SetPos(Vec2(cam->X() + 10, cam->Y()));
-            break;
-        case SDL_SCANCODE_A:
-            cam->SetPos(Vec2(cam->X() - 10, cam->Y()));
-            break;
-        case SDL_SCANCODE_W:
-            cam->SetPos(Vec2(cam->X(), cam->Y() - 10));
-            break;
-        case SDL_SCANCODE_S:
-            cam->SetPos(Vec2(cam->X(), cam->Y() + 10));
-            break;
-        case SDL_SCANCODE_KP_PLUS:
-            Window::SetMode(Window::GetWidth() + 10, Window::GetHeight() + 5, false);
-            break;
-        case SDL_SCANCODE_KP_MINUS:
-            Window::SetMode(Window::GetWidth() - 10, Window::GetHeight() - 5, false);
-            break;
+            case SDL_SCANCODE_D:
+                cam->SetPos(Vec2(cam->X() + 10, cam->Y()));
+                break;
+            case SDL_SCANCODE_A:
+                cam->SetPos(Vec2(cam->X() - 10, cam->Y()));
+                break;
+            case SDL_SCANCODE_W:
+                cam->SetPos(Vec2(cam->X(), cam->Y() - 10));
+                break;
+            case SDL_SCANCODE_S:
+                cam->SetPos(Vec2(cam->X(), cam->Y() + 10));
+                break;
+            case SDL_SCANCODE_KP_PLUS:
+                Window::SetMode(Window::GetWidth() + 10, Window::GetHeight() + 5, false);
+                break;
+            case SDL_SCANCODE_KP_MINUS:
+                Window::SetMode(Window::GetWidth() - 10, Window::GetHeight() - 5, false);
+                break;
         }
 
     }
@@ -109,15 +116,15 @@ void Engine::OnEvent(SDL_Event* event, const Uint8* keyboardState) {
 
 void Engine::OnUpdate() {
     //std::cout << "FPS: " << int_to_str(fps.GetFPS()) << '\n';
-    box->SetText( std::string("FPS: ") + std::string(int_to_str(fps.GetFPS())) );
+    box->SetText(std::string("FPS: ") + std::string(int_to_str(fps.GetFPS())));
 }
 
 void Engine::OnRender() {
     fps.OnUpdate();
     ///test_map.OnRender(0, 0);
-    SDL_Rect rct = {100, 100, 300, 300};
+    // SDL_Rect rct = {100, 100, 300, 300};
 
-    Surface::DrawRect(&rct, 100, 100, 100, 60);
+    // Surface::DrawRect(&rct, 100, 100, 100, 60);
 }
 
 void Engine::OnCleanUp() {
@@ -125,6 +132,7 @@ void Engine::OnCleanUp() {
 }
 
 #undef main
+
 int main() {
     Engine engine;
     engine.Start();
