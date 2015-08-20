3b709ee148f6a8f0749d8493a36dbacab4ef5606
WinPooh32 <winpooh32@yandex.ru>
2015-08-20 10:17:59 +1000

    Added Button widget

---
+++ src/GUI/Widgets/Button.cpp
@@ -0,0 +1,61 @@
+//
+// Created by winpooh on 16.08.15.
+//
+
+#include "Button.h"
+
+Button::Button(Object* owner, const Vec2& pos, const Vec2& size, const std::string& font, int font_pt_size) :
+        Box(owner, pos, size, font, font_pt_size){
+    ShowBack(true);
+}
+
+Button::~Button(){
+
+}
+
+void Button::SetStyle(const std::string& style, const Vec2& pos_normal, const Vec2& pos_pressed, int tile_resolution = 8) {
+    _back_style = style;
+    _pos_normal = pos_normal;
+    _pos_pressed = pos_pressed;
+    _tile_resolution = tile_resolution;
+}
+
+void Button::OnUpdate(){
+    CheckClick(GUI::GetCamera());
+    if(_clicked){
+        if(_state == BUTTON_NORMAL || _state == BUTTON_RELEASED){
+            _state = BUTTON_HOVERED;
+            SetBackGround(_back_style, _pos_pressed, _tile_resolution);
+        }else if(_state == BUTTON_HOVERED){
+
+        }
+        _clicked = false;
+    }else{
+        if(_state == BUTTON_HOVERED){
+            _state = BUTTON_RELEASED;
+            Action();
+        }else if(_state == BUTTON_RELEASED){
+            _state = BUTTON_NORMAL;
+            SetBackGround(_back_style, _pos_normal, _tile_resolution);
+        }
+    }
+
+}
+
+void Button::SetPos(const Vec2& pos){
+    Object::SetPos(pos);
+
+    //Justify text to center
+    _text_draw_rect = {GetPos().x + (GetSize().x - TEXT_OFFSET)/2 - _text_size.x/2,
+                       GetPos().y + (GetSize().y - TEXT_OFFSET)/2 - _text_size.y/2,
+                       GetSize().x - TEXT_OFFSET,
+                       GetSize().y - TEXT_OFFSET};
+}
+
+void  Button::OnClick(){
+    _clicked = true;
+}
+
+void Button::Action(){
+    std::cout << "REAL CLICK!" << std::endl;
+}
---
+++ src/GUI/Widgets/Button.h
@@ -0,0 +1,38 @@
+//
+// Created by winpooh on 16.08.15.
+//
+
+#ifndef ENGINE_BUTTON_H
+#define ENGINE_BUTTON_H
+
+#include "GUI/Widgets/Box.h"
+
+enum button_state{
+    BUTTON_NORMAL = 0,
+    BUTTON_RELEASED,
+    BUTTON_HOVERED
+};
+
+class Button: public Box {
+public:
+    Button(Object* owner, const Vec2& pos, const Vec2& size, const std::string& font, int font_pt_size);
+    virtual ~Button();
+
+    void SetStyle(const std::string& style, const Vec2& pos_normal, const Vec2& pos_pressed, int tile_resolution);
+
+    virtual void OnUpdate();
+    virtual void SetPos(const Vec2& pos);
+    virtual void OnClick();
+    virtual void Action();
+
+private:
+    bool _clicked;
+    button_state _state = BUTTON_NORMAL;
+    std::string _back_style;
+    Vec2 _pos_normal;
+    Vec2 _pos_pressed;
+    int _tile_resolution;
+};
+
+
+#endif //ENGINE_BUTTON_H
