2f7adbd463e42610c2b4119b6814a30dea2fd487
WinPooh32 <winpooh32@yandex.ru>
2015-12-13 14:32:23 +1000

    Dot product, indent fix, wrong product removed

--- src/Core/Vec2.h
+++ src/Core/Vec2.h
@@ -1,28 +1,23 @@
-/*
- * Vec2.h
- *
- *  Created on: 25 авг. 2014 г.
- *      Author: snickers
- */
-
 #ifndef VEC2_H_
 #define VEC2_H_
 
 #include <cmath>
-const float GRADUS = 3.14159265359f/180;
+const static float GRADUS = 3.14159265359f / 180;
 
 class Vec2 {
 public:
-	Vec2();
-	Vec2(const float& x, const float& y);
+    Vec2();
+    Vec2(const float& x, const float& y);
 
-	float x,y;
-	float GetLength() const;
+    float x, y;
+    float GetLength() const;
 
-	Vec2 GetNormalized() const;
-	Vec2 GetRotated(const double& angle) const;
+    Vec2 GetNormalized() const;
+    Vec2 GetRotated(const double& angle) const;
 
-	Vec2& operator=(const Vec2& right);
+    const float& Dot(const Vec2& right);
+
+    Vec2& operator=(const Vec2& right);
     bool operator==(const Vec2& right) const;
 
     friend const Vec2 operator+(const Vec2& left, const Vec2& right);
@@ -31,8 +26,7 @@ public:
     friend float operator*(const Vec2& left, const Vec2& right);
     friend Vec2& operator+=(Vec2& left, const Vec2& right);
     friend Vec2& operator-=(Vec2& left, const Vec2& right);
-    friend Vec2& operator*=(Vec2& left, const Vec2& right);
-    friend Vec2& operator*=(Vec2& left,  const float& scale);
+    friend Vec2& operator*=(Vec2& left, const float& scale);
 };
 
 #endif /* VEC2_H_ */
