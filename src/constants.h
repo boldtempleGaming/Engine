52795d27af08a1bac8f8027de679c7cdb4f0abc5
WinPooh32 <winpooh32@yandex.ru>
2016-01-31 15:51:53 +1000

    Static constants

--- src/constants.h
+++ src/constants.h
@@ -12,27 +12,26 @@
 #include <SDL2/SDL.h>
 
 #include "GUI/colors.h"
-//---------Константы---------
 
-const bool MAX_FPS = false;//false;
+const static bool MAX_FPS = false;//false;
 
-const int LOGIC_WIN_WIDTH = 800;
-const int LOGIC_WIN_HEIGHT = 400;
+const static int LOGIC_WIN_WIDTH = 800;
+const static int LOGIC_WIN_HEIGHT = 400;
 
-const int MAP_WIDTH = 20;
-const int MAP_HEIGHT = 100;
+const static int MAP_WIDTH = 20;
+const static int MAP_HEIGHT = 100;
 
-const int TILE_SHEET_SIZE = 16;
-const int TILE_SIZE = 32;
+const static int TILE_SHEET_SIZE = 16;
+const static int TILE_SIZE = 32;
 
-const std::string SPRITES_PATH = "../Data/Sprites/";
-const std::string FONTS_PATH = "../Data/Fonts/";
-const std::string SOUNDS_PATH = "../Data/Sounds/";
+const static std::string SPRITES_PATH = "../Data/Sprites/";
+const static std::string FONTS_PATH = "../Data/Fonts/";
+const static std::string SOUNDS_PATH = "../Data/Sounds/";
 
-const SDL_Color BACKGROUND_COLOR = COLOR_GRAY;
+const static SDL_Color BACKGROUND_COLOR = COLOR_GRAY;
 
-const std::string DEFAULT_FONT = "PressStart2P.ttf";
-const int DEFAULT_PTSIZE = 9;
-//---------------------------
+const static std::string DEFAULT_FONT = "PressStart2P.ttf";
+const static int DEFAULT_PTSIZE = 9;
+
 #endif /* CONSTANTS_H_ */
 
