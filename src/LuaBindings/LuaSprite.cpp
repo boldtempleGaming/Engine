9afc7f7870bd051f0d1d7eca00bc29326b2758b4
WinPooh32 <winpooh32@yandex.ru>
2017-02-03 14:36:56 +1000

    Initial sprite binding

---
+++ src/LuaBindings/LuaSprite.cpp
@@ -0,0 +1,45 @@
+#include "LuaSprite.h"
+#include <Render/Sprite.h>
+class Animation;
+class Sprite;
+
+void LuaSprite::bind(sol::state &lua){
+
+//    lua.new_simple_usertype<SDL_RendererFlip>("Flip",
+//            "NONE", sol::readonly_property(SDL_FLIP_NONE),
+//            "HORIZONTAL", sol::readonly_property(SDL_FLIP_HORIZONTAL),
+//            "VERTICAL", sol::readonly_property(&SDL_FLIP_VERTICAL)
+//    );
+
+    lua.new_enum("Flip",
+                 "NONE", SDL_FLIP_NONE,
+                 "HORIZONTAL", SDL_FLIP_HORIZONTAL,
+                 "VERTICAL", SDL_FLIP_VERTICAL
+    );
+
+    lua.new_usertype<Animation>("Animation",
+            "rate", sol::property(&Animation::GetFrameRate, &Animation::SetFrameRate),
+            "current", sol::property(&Animation::GetCurrentFrame, &Animation::SetCurrentFrame),
+            "begin", sol::property(&Animation::GetBeginFrame, &Animation::SetBeginFrame),
+            "max", sol::property(&Animation::GetMaxFrame, &Animation::SetMaxFrame)
+    );
+
+    // make usertype metatable
+    lua.new_usertype<Sprite>( "Sprite",
+
+            // 1 constructor
+            sol::constructors<sol::types<>>(),
+
+            "setTexture", sol::resolve<void(const std::string&)>(&Sprite::SetTexture),
+            "draw", sol::resolve<void(const Vec2&, const Vec2&)>(&Sprite::Draw),
+
+            // gets or set the value using member variable syntax
+            "flip", sol::property(&Sprite::GetFlip, &Sprite::SetFlip),
+            "angle", sol::property(&Sprite::GetAngle, &Sprite::SetAngle),
+
+            "setFrameSize", &Sprite::SetFrameSize,
+            "setAnimation", sol::resolve<void(const Animation&)>(&Sprite::SetAnimation)
+
+           // sol::meta_function::equal_to, sol::overload(sol::resolve<bool(const Vec2&) const>( &Vec2::operator ==  ))
+    );
+}
---
+++ src/LuaBindings/LuaSprite.h
@@ -0,0 +1,10 @@
+#ifndef LUASPRITE_H
+#define LUASPRITE_H
+
+#include <sol.hpp>
+
+namespace LuaSprite {
+    void bind(sol::state& lua);
+}
+
+#endif // LUASPRITE_H
