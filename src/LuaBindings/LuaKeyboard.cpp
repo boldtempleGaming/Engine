5845183e4064036fd1e73ac88ceff0dbd6a7a488
WinPooh32 <winpooh32@yandex.ru>
2017-03-08 21:05:40 +1000

    Added lua keyboad binding

---
+++ src/LuaBindings/LuaKeyboard.cpp
@@ -0,0 +1,6 @@
+#include "LuaKeyboard.h"
+
+LuaKeyboard::LuaKeyboard()
+{
+
+}
---
+++ src/LuaBindings/LuaKeyboard.h
@@ -0,0 +1,11 @@
+#ifndef LUAKEYBOARD_H
+#define LUAKEYBOARD_H
+
+
+class LuaKeyboard
+{
+public:
+    LuaKeyboard();
+};
+
+#endif // LUAKEYBOARD_H
