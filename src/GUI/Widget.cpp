6a14d09e57b4636b9ff0976f29dcaf674995bc64
WinPooh32 <winpooh32@yandex.ru>
2017-04-04 22:05:50 +1000

    Refactoring and fixes

--- src/GUI/Widget.cpp
+++ src/GUI/Widget.cpp
@@ -34,6 +34,7 @@ void Widget::ShowBack(bool show_bg){
 void Widget::SetBackGround(const std::string& tileset,const Vec2& skin, int tile_size) {
     if(_back.GetTexture() != nullptr){
         SDL_DestroyTexture(_back.GetTexture());
+        _back.SetTexture(nullptr);
     }
 
     //TODO FIX THIS
@@ -54,7 +55,6 @@ void Widget::SetBackGround(const std::string& tileset,const Vec2& skin, int tile
                                                   rect.h);
     SDL_SetTextureBlendMode(texture_back, SDL_BLENDMODE_BLEND);
 
-    //SDL_Texture *texture_skin = Resources::GetTexture(tileset);
     Surface::GetSkinnedRect(texture_skin, texture_back, &skin, &rect, tile_size);
 
     _back.SetTexture(texture_back);
@@ -62,6 +62,14 @@ void Widget::SetBackGround(const std::string& tileset,const Vec2& skin, int tile
 
 void Widget::SetCamera(Camera *cam){
     _camera = cam;
+
+    auto children = GetChildrenList();
+    for(auto it = children.begin(); it != children.end(); ++it){
+        Widget* wgt = dynamic_cast<Widget*>(*it);
+        if(wgt){
+            wgt->SetCamera(cam);
+        }
+    }
 }
 
 void Widget::Connect(Object *obj){
@@ -69,7 +77,57 @@ void Widget::Connect(Object *obj){
 
     Widget* wgt = dynamic_cast<Widget*>(obj);
     if(wgt){
-        wgt->_camera = this->_camera;
+        wgt->SetCamera(this->_camera);
+    }
+}
+
+void Widget::OnTopMouseEvent(){
+    if(Mouse::AnyPressed()){
+        if(Mouse::Pressed(MOUSE_LEFT)){
+            if ((_state == WIDGET_HOVERED || _state == WIDGET_NORMAL)){
+                EmitAction("mousedown");
+
+                _state = WIDGET_PRESSED;
+            }
+        }
+    }else if(_state == WIDGET_PRESSED){
+            EmitAction("mouseup");
+            _state = WIDGET_NORMAL;
+    }
+    else if(_state != WIDGET_HOVERED){
+        EmitAction("mousehover");
+        _state = WIDGET_HOVERED;
+    }
+
+    _was_intersected = true;
+}
+
+void Widget::OnMouse(){
+    if(Mouse::Wheeled(MOUSE_WHEEL_UP)){
+        EmitAction("wheelup");
+    }
+    else if(Mouse::Wheeled(MOUSE_WHEEL_DOWN)){
+        EmitAction("wheeldown");
+    }
+
+    _intersected = true;
+}
+
+void Widget::OnUpdate(){
+    if (!IsVisible() || IsClickIgnored()) {
+        return;
+    }
+
+    CheckTop(_camera);
+
+    if(_was_intersected && !_intersected){
+        EmitAction("mouseleave");
+        _was_intersected = false;
+        _state = WIDGET_NORMAL;
+    }
+
+    if(_intersected){
+       _intersected = false;
     }
 }
 
@@ -79,4 +137,61 @@ const bool& Widget::IsVisible() const{
 
 const bool& Widget::BackIsVisible() const{
     return _bg_visible;
+}
+
+void Widget::AddAction(const std::string& action_name, std::function<void ()> action, Object* listener){
+    size_t s = _Listeners.size();
+    bool found = false;
+
+    for(size_t i = 0; i < s; ++i){
+        if(_Listeners[i].listener == listener){
+            _Listeners[i].Actions[action_name] = action;
+            found = true;
+            break;
+        }
+    }
+
+    if(!found){
+        Actions actions;
+        actions.listener = listener;
+        actions.Actions[action_name] = action;
+
+        _Listeners.push_back(actions);
+    }
+}
+
+void Widget::DeleteAction(const std::string& action_name, Object* listener){
+    size_t s = _Listeners.size();
+
+    for(size_t i = 0; i < s; ++i){
+        if(_Listeners[i].listener == listener){
+            if(_Listeners[i].Actions.count(action_name)){
+                _Listeners[i].Actions.erase(action_name);
+                break;
+            }
+        }
+    }
+}
+
+void Widget::DeleteAllActions(Object* listener){
+    for(auto it = _Listeners.begin(); it != _Listeners.end(); ++it){
+        if(it->listener == listener){
+            _Listeners.erase(it);
+            break;
+        }
+    }
+}
+
+void Widget::EmitAction(const std::string& action_name){
+    size_t s = _Listeners.size();
+
+    for(size_t i = 0; i < s; ++i){
+       if(_Listeners[i].Actions.count(action_name)){
+           auto fun = _Listeners[i].Actions[action_name];
+
+           if(fun){
+               fun();
+           }
+       }
+    }
 }
--- src/GUI/Widget.h
+++ src/GUI/Widget.h
@@ -9,11 +9,23 @@
 #ifndef SRC_GUI_Widget_H_
 #define SRC_GUI_Widget_H_
 
-#include "Core/Vec2.h"
-#include "Core/Object.h"
-#include <Core/Resources.h>
-#include "Render/Sprite.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <functional>
 
+#include <Core/Vec2.h>
+#include <Core/Object.h>
+#include <Core/Resources.h>
+#include <Render/Sprite.h>
+#include <IO/Mouse.h>
+
+enum widget_state{
+    WIDGET_NORMAL = 0,
+    WIDGET_RELEASED,
+    WIDGET_HOVERED,
+    WIDGET_PRESSED
+};
 
 class Widget: public Object {
 public:
@@ -21,19 +33,41 @@ public:
     virtual ~Widget();
     void Show(bool show);
     void ShowBack(bool show_bg);
-    void SetBackGround(const std::string& tileset, const Vec2& skin, int tile_size);
-    void SetCamera(Camera* cam);
+    virtual void SetBackGround(const std::string& tileset, const Vec2& skin, int tile_size);
+    virtual void SetCamera(Camera* cam);
+
+    void AddAction(const std::string& action_name, std::function<void(void)> action, Object* listener);
+    void DeleteAction(const std::string& action_name, Object* listener);
+    void DeleteAllActions(Object* listener);
 
     virtual void Connect(Object *obj);
+    virtual void OnTopMouseEvent();
+    virtual void OnMouse();
+    virtual void OnUpdate();
 
     const bool& IsVisible() const;
     const bool& BackIsVisible() const;
 
 protected:
+    typedef  struct {Object* listener;
+                     std::unordered_map<std::string, std::function<void(void)>> Actions;} Actions;
+
     Camera* _camera = nullptr;
+    Sprite _back;
+
+    widget_state _prev_state = WIDGET_NORMAL;
+    widget_state _state = WIDGET_NORMAL;
+
     bool _visible;
     bool _bg_visible;
-    Sprite _back;
+    bool _clicked = false;
+    bool _was_intersected = false;
+    bool _intersected = false;
+
+    std::function<void(void)> _click_action;
+    std::vector<Actions> _Listeners;
+
+    void EmitAction(const std::string& action_name);
 };
 
 #endif /* SRC_GUI_Widget_H_ */
