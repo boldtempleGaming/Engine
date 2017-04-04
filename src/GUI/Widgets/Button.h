52d35cacc6376e6fc8e4d414f570d2ab2dc916ca
WinPooh32 <winpooh32@yandex.ru>
2017-04-04 22:05:31 +1000

    Button refactoring and fixes

--- src/GUI/Widgets/Button.h
+++ src/GUI/Widgets/Button.h
@@ -4,39 +4,24 @@
 #include <functional>
 #include "GUI/Widgets/Box.h"
 
-enum button_state{
-    BUTTON_NORMAL = 0,
-    BUTTON_RELEASED,
-    BUTTON_HOVERED
-};
-
 class Button: public Box {
 public:
     Button(const Vec2& pos, const Vec2& size, const std::string& font, int font_pt_size);
     virtual ~Button();
 
-    void SetStyle(const std::string& style, const Vec2& pos_normal, const Vec2& pos_pressed, int tile_resolution);
-    void RegisterAction(std::function<void(void)> action);
-   // void RegisterAction(std::Mem_fn<void(void)> mem_action);
-
-    void SetText(const std::string& str);
-
-    virtual void OnUpdate();
-    virtual void SetPos(const Vec2& pos);
-    virtual void OnClick();
-    virtual void Action();
-
-protected:
-    std::function<void(void)> _action;
+    void SetStyle(const std::string& style,
+                  const Vec2& pos_normal,
+                  const Vec2& pos_pressed,
+                  const Vec2 &pos_hovered,
+                  int tile_resolution);
+    virtual void SetText(const std::string& str);
 
 private:
-    bool _clicked;
-    button_state _state = BUTTON_NORMAL;
     std::string _back_style;
     Vec2 _pos_normal;
     Vec2 _pos_pressed;
+    Vec2 _pos_hovered;
     int _tile_resolution;
 };
-
 
 #endif //ENGINE_BUTTON_H
