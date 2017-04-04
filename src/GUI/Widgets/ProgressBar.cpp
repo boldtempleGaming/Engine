d28aad69077834412a2ce96fb26f41088b38ab63
WinPooh32 <winpooh32@yandex.ru>
2017-04-04 22:02:47 +1000

    Progress bar initial implementation

---
+++ src/GUI/Widgets/ProgressBar.cpp
@@ -0,0 +1,6 @@
+#include "ProgressBar.h"
+
+ProgressBar::ProgressBar()
+{
+
+}
---
+++ src/GUI/Widgets/ProgressBar.h
@@ -0,0 +1,11 @@
+#ifndef PROGRESSBAR_H
+#define PROGRESSBAR_H
+
+
+class ProgressBar : public Widget
+{
+public:
+    ProgressBar();
+};
+
+#endif // PROGRESSBAR_H
