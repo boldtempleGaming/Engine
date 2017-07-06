23741cd5746a95209fcd0ffdf34f0505f60c96ff
Unknown <winpooh32@yandex.ru>
2017-07-06 14:19:49 +1000

    CleaupAndUpload

--- src/Core/Vec2.cpp
+++
@@ -1,8 +0,0 @@
-#include "Vec2.h"
-
-//Const defs often used vectors:
-const Vec2 Vec2::ZERO = Vec2(0.0f, 0.0f);
-const Vec2 Vec2::UP = Vec2(0.0f, -1.0f);
-const Vec2 Vec2::DOWN = Vec2(0.0f, 1.0f);
-const Vec2 Vec2::LEFT = Vec2(-1.0f, 0.0f);
-const Vec2 Vec2::RIGHT = Vec2(1.0f, 0.0f);
