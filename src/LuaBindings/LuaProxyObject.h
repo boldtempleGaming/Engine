60b4e39ec3fe695c351c29faca86f17042ba5811
Unknown <winpooh32@yandex.ru>
2017-07-16 21:37:27 +1000

    Fixed object instantiation from Lua

--- src/LuaBindings/LuaProxyObject.h
+++ src/LuaBindings/LuaProxyObject.h
@@ -8,20 +8,22 @@ class LuaProxyObject: public Object{
 public:
     static void bind(sol::state& lua);
 
-    LuaProxyObject(sol::table& lua_table, const sol::function& fun_delete);
+    LuaProxyObject(sol::table lua_table, sol::function fun_delete);
     virtual ~LuaProxyObject();
 
+    void OnPostInit();
     virtual void OnUpdate();
     virtual void OnRender();
     virtual void OnTopMouseEvent();
     virtual void OnCollide(Object* obj);
 
 private:
-    void BindScriptFunctions(sol::table& lua_table, const sol::function &fun_delete);
+    void BindScriptFunctions(sol::table& lua_table, sol::function &fun_delete);
 
     std::unordered_map<std::string, std::string> _props;
 
     sol::function _fun_init;
+    sol::function _fun_postinit;
     sol::function _fun_update;
     sol::function _fun_render;
     sol::function _fun_collide;
